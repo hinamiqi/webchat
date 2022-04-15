f0a32dda7f34846a225d5d1996cbf2900902f370
DA <hinamiqi@gmail.com>
2022-04-15 21:42:38 +0300

    BasicChat: Simple avatars
    In this patch we added basic user avatars.
    They are generated with the help of
    identicon.js for now.

--- frontend/src/app/common/components/chat-message/chat-message.component.ts
+++ frontend/src/app/common/components/chat-message/chat-message.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+import Identicon from 'identicon.js';
 
 import { IMessageView } from 'src/app/models/message/message.interface';
 
@@ -8,10 +11,22 @@ import { IMessageView } from 'src/app/models/message/message.interface';
   styleUrls: ['./chat-message.component.scss']
 })
 
-export class ChatMessageComponent {
+export class ChatMessageComponent implements OnInit {
   @Input() message: IMessageView;
 
   @Output() removed = new EventEmitter<IMessageView>();
+
+  imageSource: SafeResourceUrl;
+
+  constructor(
+    private readonly sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+      let identicon = new Identicon(this.message.author.uuid, 50);
+
+      this.imageSource = this.sanitizer.bypassSecurityTrustUrl(`data:image/png;base64,${identicon.toString()}`);
+  }
 
   remove(): void {
     this.removed.emit(this.message);
