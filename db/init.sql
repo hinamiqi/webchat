4c05cddead15f5be24f8cdb2e524cab36675a9c0
DA <hinamiqi@gmail.com>
2022-10-09 21:20:07 +0300

    Db: init script

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,10 @@
+\i ./sql/create_auto_sequence.sql
 \i ./sql/create_users.sql
 \i ./sql/create_roles.sql
 \i ./sql/create_user_roles.sql
 \i ./sql/create_chat_message.sql
 \i ./sql/create_message_reply_link.sql
+\i ./sql/create_images.sql
+\i ./sql/create_memes.sql
+\i ./sql/create_message_meme_link.sql
+\i ./sql/create_user_avatar_link.sql
