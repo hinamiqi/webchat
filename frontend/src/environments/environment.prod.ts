57616fb575466ac3367faa0f57c5552a8d89b3fb
DA <hinamiqi@gmail.com>
2022-10-12 21:06:36 +0300

    Fixed prod environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://localhost:8080/api',
-  websocketUrl: 'ws://localhost:8080/api/ws'
+  backendUrl: './api',
+  websocketUrl: './api/ws'
 };
